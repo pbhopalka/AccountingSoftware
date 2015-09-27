16be069edd0a42d62cd5f2f371efc9c9ba7888ed
Piyush Bhopalka <pbhopalka@gmail.com>
2015-09-28 01:32:18 +0530

    Make the admin interface proper

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+# For logging in to admin: username:pbhopalka and passwd:NORMAL
+
+from .models import C_Details, Manager
+
+# Use admin.StackInline or admin.TabularInline if we have foreign keys and needs
+# to be included in one table. Check https://docs.djangoproject.com/en/1.8/intro/tutorial02/#adding-related-objects
+
+class C_Details_Admin(admin.ModelAdmin):
+    fieldsets = [
+        (None,          {'fields':['CustName']}),
+        ('Address',     {'fields':['AddStreet', 'AddDistrict', 'AddState']}),
+        ('Contact Info',{'fields':['Email', 'Phone']}),
+        (None,          {'fields':['PendingAmount']}),
+    ]
+    # See https://docs.djangoproject.com/en/1.8/intro/tutorial02/#customize-the-admin-change-list
+    list_display = ('CustID', 'CustName', 'PendingAmount', 'is_safe_state')
+
+class Manager_Admin(admin.ModelAdmin):
+    fieldsets = [
+        (None,          {'fields':['ManaName']}),
+        (None,          {'fields':['ManaAuth']}),
+        ('Credentials', {'fields':['username', 'password'], 'classes':['collapse']}),
+    ]
+    list_display = ('ManaID', 'ManaName', 'ManaAuth')
+
+
+admin.site.register(C_Details, C_Details_Admin)
+admin.site.register(Manager, Manager_Admin)
--- accounting/models.py
+++ accounting/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+limitAmount = 1000
 
 class C_Details(models.Model):
     CustID = models.AutoField(primary_key=True)
@@ -12,9 +13,20 @@ class C_Details(models.Model):
     Phone = models.CharField(max_length=10)
     PendingAmount = models.IntegerField(default=0)
 
+
+    def __unicode__(self):
+        return u'%d %s' %(self.CustID, self.CustName)
+
+    def is_safe_state(self):
+        return self.PendingAmount < limitAmount
+    is_safe_state.short_description = 'Can take credit?'
+
 class Manager(models.Model):
     ManaID = models.AutoField(primary_key=True)
     ManaName = models.CharField(max_length=200)
     ManaAuth = models.CharField(max_length=10, default='Normal') #can be normal or superuser
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.ManaName
